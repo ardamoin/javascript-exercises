a7f708a73a36ae2472d8d5beedc17252562dff1d
ardamoin <ardamoin2@gmail.com>
2022-10-31 22:50:26 -0400

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+  return arr.reduce((prev, curr) => {
+    return prev + curr;
+  }, 0);
 	
 };
 
-const subtract = function() {
+const multiply = function(arr) {
+  return arr.reduce((prev, curr) => {
+    return prev * curr;
+  }, 1);
+};
+
+const power = function(a, b) {
+  return a**b;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 1 || num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num-1);
+  }
 };
 
 // Do not edit below this line
